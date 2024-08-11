d5fed395d89113be0e1cd4fd26e72cf6bec5c0ff
Cristi Moldovan <cristimc8@gmail.com>
2024-08-11 20:57:06 +0300

    chore: colorscheme for transparent bg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,53 @@
-return {}
--- return {
---   {
---     "folke/tokyonight.nvim",
---     opts = {
---       transparent = true,
---       styles = {
---         sidebars = "transparent",
---         floats = "transparent",
---       },
---     },
---   },
--- }
+-- return {}
+return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = false,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local colors = require("cyberdream.colors").default
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = false,
+
+        theme = {
+          highlights = {
+            Constant = { fg = colors.magenta },
+          },
+        },
+      })
+      -- vim.cmd("colorscheme cyberdream")
+    end,
+  },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  {
+    "ray-x/aurora",
+    init = function()
+      vim.g.aurora_italic = 1
+      vim.g.aurora_transparent = 1
+      vim.g.aurora_bold = 1
+    end,
+    config = function()
+      -- override defaults
+      vim.api.nvim_set_hl(0, "@number", { fg = "#e933e3" })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
