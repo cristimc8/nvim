0bcead5920f3c000635a11fe9bbcfd954f8e6e09
Cristi Moldovan <cristimc8@gmail.com>
2024-09-03 20:58:26 +0300

    chore: sync

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -45,9 +45,19 @@ return {
     end,
   },
   {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      -- vim.cmd([[colorscheme tokyodark]])
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "tokyodark",
     },
   },
 }
