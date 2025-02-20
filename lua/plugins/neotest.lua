d50607e653c1d13840dbd6dca8c3d19841052821
Cristi Moldovan <cristimc8@gmail.com>
2025-02-20 12:44:24 +0200

    test: neotest config working for nx workspaces

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -18,19 +18,29 @@ return {
 
       -- Function to find jest config file, either jest-e2e.json or jest.config.json
       local function find_jest_config_file()
-        local e2e_config = vim.fn.findfile("jest-e2e.json", vim.fn.expand("%:p:h") .. ";")
-        if e2e_config ~= "" then
-          return vim.fn.fnamemodify(e2e_config, ":p")
-        else
-          return "jest.config.json"
+        local search_paths = {
+          "jest-e2e.json",
+          "jest.config.json",
+          "jest.config.ts",
+          "jest.config.js",
+        }
+
+        for _, file in ipairs(search_paths) do
+          local found_file = vim.fn.findfile(file, vim.fn.expand("%:p:h") .. ";")
+          if found_file ~= "" then
+            return vim.fn.fnamemodify(found_file, ":p")
+          end
         end
+
+        return nil -- Return nil if no config file is found
       end
 
       require("neotest").setup({
         adapters = {
           require("neotest-jest")({
-            jestCommand = "npm test --",
+            jestCommand = "npx jest",
             jestConfigFile = find_jest_config_file(), -- Use the function to find jest config file
+            env = { CI = "true" },
             cwd = function()
               local buffer_dir = vim.fn.expand("%:p:h")
               return find_package_json_dir(buffer_dir) or vim.fn.getcwd()
